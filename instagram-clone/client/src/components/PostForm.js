fc8d0fd87438fff325c803b879c098ee9214f901
gkatjddl <alswjddn1002@gmail.com>
2024-04-22 21:18:53 +0900

    4/22

--- instagram-clone/client/src/components/PostForm.js
+++ instagram-clone/client/src/components/PostForm.js
@@ -1,6 +1,11 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+// 서버 요청 처리
 import { createPost } from "../service/post";
 import { FaCamera } from "react-icons/fa6";
 
-export default function PostForm() {};
+export default function PostForm() {
+  const [files, setFiles] = useState([]);
+  const [caption, setCaption] = useState("");
+  const navigate = useNavigate();
+};
